76fb3d47f42a757f37582b7a8c9425b945e37509
Lengthrequired <hyp3rcr4ft@gmail.com>
2023-01-15 00:51:57 +0100

    FIX 2.99

--- src/main/java/vote/Client/AppVote.java
+++ src/main/java/vote/Client/AppVote.java
@@ -600,7 +600,7 @@ public class AppVote extends Application {
                         btn2.setDisable(true);
                         vBox.getChildren().remove(labelDejaVote);
                         System.out.println(root.getChildren().size());
-                        if (root.getChildren().size()==7 || root.getChildren().size()==6){
+                        if (root.getChildren().size()==7 || root.getChildren().size()==5){
                             root.getChildren().remove(StackVote);
                             root.getChildren().add(StackVote);
                         }
